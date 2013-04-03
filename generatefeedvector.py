cb5a0d7b592e8778800926dd4dc750c772df5651
Ryan Allen <ryan.michael.allen@gmail.com>
2013-04-03 16:32:10 -0500

    random update

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -1,62 +1,53 @@
 import feedparser
 import re
 
-#Returns title and dictionary of word counds for an RSS feed
 def getwordcounts(url):
-  # Parse the feed
-  d=feedparser.parse(url)
-  wc={}
+    d=feedparser.parse(url)
+    wc={}
 
-  # Loop over all the entries
-  for e in d.entries:
-    if 'summary' in e: summary=e.summary
-    else: summary=e.description
+    for e in d.entries:
+        if 'summary' in e: summary=e.summary
+        else: summary=e.description
 
-    # Extract a list of words
-    words=getwords(e.title+' '+summary)
-    for word in words:
-      wc.setdefault(word,0)
-      wc[word]+=1
-  try:
-      return d.feed.title,wc
-  except AttributeError:
-      return 'nofeed',wc
+        words=getwords(e.title+' '+summary)
+        for word in words:
+            wc.setdefault(word,0)
+            wc[word]+=1
+    return d.feed.title,wc
 
-#Gets words from parse
 def getwords(html):
-    #remove all HTML
-    txt=re.compile(r'[^>]+>').sub('',html)
-    #split words by all non-alpha characters
-    words=re.compile(r'^A-Z^a-z]+').split(txt)
-    #convert to lowercase
-    return[word.lower() for word in words if word!='']
+    txt=re.compile(r'<[^>]+>').sub('',html)
 
+    words=re.compile(r'[^A-Z^a-z]+').split(txt)
+
+    return [word.lower() for word in words if word!='']
 
 apcount={}
 wordcounts={}
-feedlist=[]
-for feedurl in file('feedlist.txt'):
-    feedlist.append(feedurl)
-    title,wc=getwordcounts(feedurl)
-    wordcounts[title]=wc
-    for word,count in wc.items():
-        apcount.setdefault(word,0)
-        if count>1:
-            apcount[word]+=1
+feedlist=[line for line in file('feedlist.txt')]
+for feedurl in feedlist:
+    try:
+        title,wc=getwordcounts(feedurl)
+        wordcounts[title]=wc
+        for word,count in wc.items():
+            apcount.setdefault(word,0)
+            #
+            if count>1:
+                apcount[word]+=1
+    except:
+        print 'Failed to parse feed %s' % feedurl
 
 wordlist=[]
 for w,bc in apcount.items():
     frac=float(bc)/len(feedlist)
-    if frac>0.1 and frac<0.5: wordlist.append(w)
+    if frac>0.2 and frac<0.3: wordlist.append(w)
 
 out=file('blogdata.txt','w')
 out.write('Blog')
 for word in wordlist: out.write('\t%s' % word)
 out.write('\n')
 for blog,wc in wordcounts.items():
-    #deal with unicode outsie the ascii range
-    blog=blog.encode('ascii', 'ignore')
-    out.write(blog)
+    out.write(blog.encode('utf-8'))
     for word in wordlist:
         if word in wc: out.write('\t%d' % wc[word])
         else: out.write('\t0')
